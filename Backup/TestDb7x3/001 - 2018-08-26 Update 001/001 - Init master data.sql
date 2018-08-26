08dcaa0754d6c3986807511b08b59f3327480b7f
chumpon-asaneerat <chumponsenate@gmail.com>
2018-08-26 18:13:31 +0700

    deploy db script to production server #1

---
+++ Backup/TestDb7x3/001 - 2018-08-26 Update 001/001 - Init master data.sql
@@ -0,0 +1,37 @@
+EXEC InitLanguages
+SELECT * FROM Language
+
+EXEC InitErrorMessages
+SELECT * FROM ErrorMessage
+SELECT * FROM ErrorMessageML
+
+EXEC InitMemberTypes
+SELECT * FROM MemberType
+SELECT * FROM MemberTypeML
+
+EXEC InitDeviceTypes
+SELECT * FROM DeviceType
+SELECT * FROM DeviceTypeML
+
+EXEC InitLimitUnits
+SELECT * FROM LimitUnit
+SELECT * FROM LimitUnitML
+
+EXEC InitPeriodUnits
+SELECT * FROM PeriodUnit
+SELECT * FROM PeriodUnitML
+
+EXEC InitMasterPKs
+SELECT * FROM MasterPK
+
+EXEC InitDefaultUser
+SELECT * FROM UserInfo
+SELECT * FROM UserInfoML
+
+EXEC InitLicenseTypes
+SELECT * FROM LicenseType
+SELECT * FROM LicenseTypeML
+
+EXEC InitLicenseFeatures
+SELECT * FROM LicenseFeature
+
