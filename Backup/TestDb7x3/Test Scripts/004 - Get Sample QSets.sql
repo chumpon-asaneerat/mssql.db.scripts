c6b2307671e53d9fd3cd33269285c6e8c319d801
chumpon-asaneerat <chumponsenate@gmail.com>
2018-08-27 10:28:50 +0700

    Add TestCode 004

---
+++ Backup/TestDb7x3/Test Scripts/004 - Get Sample QSets.sql
@@ -0,0 +1,15 @@
+﻿DECLARE @langId nvarchar(3) = N'TH'
+DECLARE @customerId nvarchar(30) = N'EDL-C2018080001'
+DECLARE @qSetId nvarchar(30) = N'QS00001'
+DECLARE @qSeq int = 1;
+
+EXEC GetQSets @langId, @customerId
+
+SET @qSetId = N'QS00001'
+EXEC GetQSlides @langId, @customerId, @qSetId
+
+SET @qSetId = N'QS00002'
+EXEC GetQSlides @langId, @customerId, @qSetId
+
+SET @qSetId = N'QS00003'
+EXEC GetQSlides @langId, @customerId, @qSetId
